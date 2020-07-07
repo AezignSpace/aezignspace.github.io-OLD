1b65421c2b658433e586c85ab21df3dd11b9ce55
Maytham <44939173+Maytha8@users.noreply.github.com>
2020-07-07 21:34:14 +0800

    Update website

--- resources/js/index.js
+++ resources/js/index.js
@@ -11,4 +11,18 @@ $(document).ready(function() {
     $("#menubtn span").text("Menu")
     $("header nav div").removeClass("open")
   })
+
+  $('a[href^="#"]').on('click', function(e) {
+    e.preventDefault()
+    var target = $(this.hash).get(0)
+    var pos = target.getBoundingClientRect().top
+    var menuheight = $("header").height()
+    var goto = pos - menuheight
+    console.log(target, pos, menuheight, goto)
+    window.scrollBy({
+      top: goto,
+      left: 0,
+      behavior: 'smooth'
+    })
+  })
 })
